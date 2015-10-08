56940f003fc795eb34c8155b703036788f03bc8a
Timiz Qi <uncledu@aliyun.com>
2015-10-08 15:09:31 +0800

    mac系统卸载JDK

---
+++ removeJdkForMac.sh
@@ -0,0 +1,3 @@
+sudo rm -rf /Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk
+sudo rm -rf /Library/PreferencePanes/JavaControlPanel.prefPane
+sudo rm -rf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin
